4a0e76caa9d2185e6faa3a3589e9820a940b66fa
Rex Dex <rexdexpl@gmail.com>
2023-09-22 20:51:52 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,30 +1,38 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Rex Dex",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Game Engine Developer",
+    bio: "I develop game engines for fun.",
+    email: "rexdexpl@gmail.com",
+    linkedin: "https://www.linkedin.com/in/tomasz-jonarski-83667a11",
+    github: "https://www.github.com/rexdex/",
     instagram: "",
   },
   projects: [
+    {      
+      name: `Inferno Engine`,
+      href: "https://github.com/rexdex/inferno",
+    },
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Xbox360 emulator`,
+      href: "https://github.com/rexdex/recompiler",
+    },
+    {
+      name: `Onion (Yet Another Build Tool)`,
+      href: "https://github.com/rexdex/onion",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Inferno Dev Blog",
+    description: "Welcome to Inferno Engine dev blog",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2019, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
@@ -47,7 +55,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
